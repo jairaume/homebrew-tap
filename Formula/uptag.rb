142608cb6c071e66e2f8513b41556d3b0378edda
Jérôme Rascle <jerome.rascle@music-story.com>
2026-03-03 16:25:55 +0100

    feat: add uptag sha256

--- Formula/uptag.rb
+++ Formula/uptag.rb
@@ -2,7 +2,7 @@ class Uptag < Formula
   desc "Interactive git tag version bumper for zsh"
   homepage "https://github.com/jairaume/uptag"
   url "https://github.com/jairaume/uptag/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "PLACEHOLDER"
+  sha256 "f4e66a0a5ddb5c51737fe108ee77a973525ca102ad8b1afd081e0cf91eb1522b"
   license "MIT"
 
   def install
